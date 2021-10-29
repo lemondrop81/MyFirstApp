b5d90c9e5eef39725211e42b62199e7945d35b9e
lemondrop81 <hacker814@hotmail.com>
2021-10-29 08:56:45 -0500

    Create function saySomething

--- app/src/main/java/com/example/myfirstapp/utilities/myClass.kt
+++ app/src/main/java/com/example/myfirstapp/utilities/myClass.kt
@@ -1,4 +1,11 @@
 package com.example.myfirstapp.utilities
 
-class myClass {
+import android.util.Log
+
+const val TAG = "my_tag"
+
+class MyClass {
+    fun saySomething(){
+        Log.d(TAG, "saySomething: Hello!")
+    }
 }
