bae58c2e14ede4c7b4b68bea5d1029273c79d8cb
lemondrop81 <hacker814@hotmail.com>
2021-10-29 10:27:48 -0500

    Convert the java files to kotlin

--- app/src/main/java/com/example/myfirstapp/MainActivity2.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity2.kt
@@ -1,51 +1,35 @@
-package com.example.myfirstapp;
+package com.example.myfirstapp
 
-import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.myfirstapp.R
+import androidx.navigation.ui.NavigationUI
+import com.example.myfirstapp.databinding.ActivityMain2Binding
+import com.google.android.material.snackbar.Snackbar
 
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.myfirstapp.databinding.ActivityMain2Binding;
-
-public class MainActivity2 extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMain2Binding binding;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        binding = ActivityMain2Binding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+class MainActivity2 : AppCompatActivity() {
+    private var appBarConfiguration: AppBarConfiguration? = null
+    private var binding: ActivityMain2Binding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        setSupportActionBar(binding!!.toolbar)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration.Builder(navController.graph).build()
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration!!)
+        binding!!.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main)
+        return (NavigationUI.navigateUp(navController, appBarConfiguration!!)
+                || super.onSupportNavigateUp())
     }
 }
